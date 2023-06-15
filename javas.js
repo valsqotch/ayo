231d33c67920769961a38d50ac2c26d36136b8a9
valsqotch <136604293+valsqotch@users.noreply.github.com>
2023-06-15 14:16:47 +0800

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,5 @@
+const d1 = document.getElementById("d1"),
+      btn = document.getElementById("btn");
+    btn.addEventListener("click",function(){
+        d1.innerHTML=`<img id="image" src="piyan.jpg"/>`;
+    })  
